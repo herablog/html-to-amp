3a538b50425723fa77e1eb88f7626c727586b0c4
hara_kazunari <hara_kazunari@cyberagent.co.jp>
2019-02-06 19:55:57 +0900

    fix: change regex to detect valid tweet entry id

--- src/plugins/twitter.js
+++ src/plugins/twitter.js
@@ -5,7 +5,7 @@ function extractTweetId(href) {
   // Extract '690077988243836928' from https://twitter.com/nodenpm/status/690077988243836928?ref_src=twsrc
   const pathname = url.parse(href).pathname || '';
   const tweetId = path.basename(pathname);
-  const isValidTweetId = /\d+/.test(tweetId);
+  const isValidTweetId = /^\d+$/.test(tweetId);
   return isValidTweetId ? tweetId : null;
 }
 
